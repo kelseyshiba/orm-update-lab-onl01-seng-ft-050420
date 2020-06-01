67a4c8d5afce2f3386439ba6721884aa3b4c2441
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-01 15:27:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,11 +42,14 @@ class Student
     end
   end
   
-  def
   def self.create
     new_student = self.new(name, grade)
     new_student.save
     new_student
+  end
+  
+  def self.new_from_db(array)
+  
   end
 
 end
